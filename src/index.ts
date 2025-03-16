24ecfdea57676dc3fd0553d6da6dd2640aa3733f
den59k <den59k@gmail.com>
2025-03-16 17:27:20 +0500

    fix: fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-export { useClickOutside } from './hooks/useClickOutside'
+export { useClickOutside, useMouseDownOutside } from './hooks/useClickOutside'
 export { useDraggableItem } from './hooks/useDraggableItem'
 export { useForm, setUseFormErrorHandler } from './hooks/useForm'
 export { usePending } from './hooks/usePending'
 export { useRequest, useRequestWatch, mutateRequestFull, mutateRequest, resetRequestCache, makeRequest } from './hooks/useRequest'
 export { useSearch, compareSearchFeed, getSearchFeed, findByKeys } from './hooks/useSearch'
 export { useParentScroll } from './hooks/useParentScroll'
-export { useKeyDownLayer } from './hooks/useKeyDownLayer'
+export { useKeyDownLayer, handleKeyDownLayer } from './hooks/useKeyDownLayer'
 export { useVModel, numberParser } from './hooks/useParserVModel'
 
 export { findParent } from './utils/findParent'
